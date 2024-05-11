2dce8f12f9971449c577b408a37a9273a4e1c92a
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2024-05-11 14:52:48 +0200

    Delete hcoptcha/setup.py

--- hcoptcha/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="hcoptcha",
-    version="0.1",
-    description="A Wrapper for hcoptcha captchs solving service",
-    packages=["hcoptcha"],
-    install_requires=["requests"],
-)
